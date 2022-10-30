65e1dd62a556c5426f87334c28d92409e0a76fe6
sunyeongan <asy100@naver.com>
2022-10-30 21:22:23 +0900

    feat : main,detail page update #2

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,342 +1,151 @@
+import 'package:amata/todo_sample.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'Screen/todd_detail_screen.dart';
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-            elevation: 0.0,
-          leading: IconButton(icon: Icon(Icons.add_circle_outline),onPressed: null),
+      home : HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  static List<String> storeName = [
+    '올리브영',
+    '이마트',
+    '일송기념도서관',
+    '다이소',
+    '공학관',
+    'CLC'
+
+
+  ];
+
+  static List<String> storeImagePath = [
+    './imgs/olive-logo.png',
+    './imgs/emart-logo.png',
+    './imgs/hallym-logo.png',
+    './imgs/daiso-log.png',
+    './imgs/olive-logo.png',
+    './imgs/hallym-logo.png'
+
+  ];
+
+  static List<String> todoText = [
+    '당근 토너패드 구매',
+    '블루베리 구매',
+    '책 2권 반납하기',
+    '이사용 단프라 박스 구매',
+    '행정실에 서류제출',
+    '학지팀 방문'
+  ];
+
+  final List<TodoSample> todoSampleData = List.generate(
+      todoText.length, (index) =>
+      TodoSample(
+          storeName[index], todoText[index], storeImagePath[index], false));
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0.0,
+          leading: IconButton(
+              icon: Icon(Icons.add_circle_outline), onPressed: null),
           actions: [
-            IconButton(icon: Icon(Icons.notifications_none_outlined), onPressed: null),
+            IconButton(
+                icon: Icon(Icons.notifications_none_outlined), onPressed: null),
           ],
-            backgroundColor: Colors.white12
+          backgroundColor: Colors.white
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+            IconButton(
+                onPressed: () {}, icon: Icon(Icons.calendar_month_outlined)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.chat_bubble_outline)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.person_outline)),
+          ],
         ),
-
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.home)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.calendar_month_outlined)),
-              IconButton(onPressed: () {}, icon : Icon(Icons.chat_bubble_outline)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.person_outline)),
-            ],
+      ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            child: Text(
+              "선영 Todo", style: TextStyle(color: Colors.black,
+                fontSize: 40,
+                fontWeight: FontWeight.bold),),
+            top: 40,
+            left: 20,
           ),
-        ),
-        body: Container(
-          margin: EdgeInsets.fromLTRB(5, 20, 5, 0),
-          child: Stack(
-            children: [
-              Positioned(
-                top : 30,
-                width : 300,
-                height: 100,
 
-                child: Text(
-                    '선영 Todo',
-                    style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),
-                ),
-              ),
-              Positioned(
-                top : 100,
-                width : 380,
-                height: 200,
-                child: Container(
-                  child: Row(
-                      children : [
-                        Container(
-                          padding:EdgeInsets.fromLTRB(15, 0, 5, 70),
-                          child : ClipRRect(
-                            borderRadius: BorderRadius.circular(45.0),
-                            child : Image.asset(
-                              'imgs/olive-logo.png',width: 70.0,
-                            ),
-
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(20),
-                          child:Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('올리브영',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,height: 2.2,),),
-                              Text('당근 토너패드 구매 외 2개',style: TextStyle(fontSize: 15,),),
-                              Row(
-                                children: [
-                                  SizedBox(
-                                    child: Container(
-                                      margin: EdgeInsets.fromLTRB(0, 10, 10, 0),
-                                      padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                      color: Colors.white70,
-                                      child: Row(
-                                        children : [
-                                          Icon(Icons.arrow_upward_outlined),
-                                          Text(' 652m',style: TextStyle(fontWeight: FontWeight.bold),)
-                                        ],
-
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    child: Container(
-                                      margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                      padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                      color: Colors.white70,
-                                      child: Row(
-                                        children : [
-                                          Icon(Icons.check_box_outlined),
-                                          Text(' 1/3',style: TextStyle(fontWeight: FontWeight.bold),)
-                                        ],
-
-                                      ),
-                                    ),
-                                  ),
-                                ],
-
-                              ),
-
-                            ],
-                          ),
-                        ),
-
-                      ],
-
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xffD6EBE8),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(22.0),
+          Container(
+            margin: EdgeInsets.only(top: 130),
+            decoration: BoxDecoration(
+              color: const Color(0xffD6EBE8),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(40),
+                topLeft: Radius.circular(40),),
+            ),
+            child: ListView.builder(
+              itemCount: todoSampleData.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: ListTile(
+                    title: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 9.0),
+                      child: Text(
+                        todoSampleData[index].storeName, style: TextStyle(
+                          color: Colors.grey[900],
+                          fontWeight: FontWeight.bold),),
                     ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 250,
-                width : 380,
-                height: 200,
-                child: Container(
-                    child: Row(
-                      children : [
-                        Container(
-                          padding:EdgeInsets.fromLTRB(15, 0, 5, 70),
-                          child : ClipRRect(
-                            borderRadius: BorderRadius.circular(45.0),
-                            child : Image.asset(
-                              'imgs/emart-logo.png',width: 70.0,
-                            ),
-
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(20),
-                          child:Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('이마트',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,height: 2.2,),),
-                              Text('블루베리 외 5개',style: TextStyle(fontSize: 15,),),
-                              Row(
-                                children: [
-                                  SizedBox(
-                                    child: Container(
-                                      margin: EdgeInsets.fromLTRB(0, 10, 10, 0),
-                                      padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                      color: Colors.white70,
-                                      child: Row(
-                                        children : [
-                                          Icon(Icons.arrow_upward_outlined),
-                                          Text(' 1826m',style: TextStyle(fontWeight: FontWeight.bold),)
-                                        ],
-
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    child: Container(
-                                      margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                      padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                      color: Colors.white70,
-                                      child: Row(
-                                        children : [
-                                          Icon(Icons.check_box_outlined),
-                                          Text(' 0/6',style: TextStyle(fontWeight: FontWeight.bold),)
-                                        ],
-
-                                      ),
-                                    ),
-                                  ),
-                                ],
-
-                              ),
-
-                            ],
-                          ),
-                        ),
-
-                      ],
+                    leading: SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: Image.asset(todoSampleData[index].imgPath),
 
                     ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xffF6F0EB),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(22.0),
-                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              TodoDetailScreen(todo: todoSampleData[index],)));
+                      debugPrint(todoSampleData[index].storeName);
+                    },
+                    subtitle: Text(
+                      todoSampleData[index].todoText, style: TextStyle(
+                      color: Colors.grey[700],),),
+                    trailing: const Icon(Icons.more_horiz),
+                    isThreeLine: true,
                   ),
-                ),
-              ),
-              Positioned(
-                top: 400,
-                width : 380,
-                height: 200,
-                child: Container(
-                  child: Row(
-                    children : [
-                      Container(
-                        padding:EdgeInsets.fromLTRB(15, 0, 5, 70),
-                        child : ClipRRect(
-                          borderRadius: BorderRadius.circular(45.0),
-                          child : Image.asset(
-                            'imgs/hallym-logo.png',width: 70.0,
-                          ),
+                );
+              },
 
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        child:Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('일송기념도서관',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,height: 2.2,),),
-                            Text('책 반납 외 1개',style: TextStyle(fontSize: 15,),),
-                            Row(
-                              children: [
-                                SizedBox(
-                                  child: Container(
-                                    margin: EdgeInsets.fromLTRB(0, 10, 10, 0),
-                                    padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                    color: Colors.white70,
-                                    child: Row(
-                                      children : [
-                                        Icon(Icons.arrow_upward_outlined),
-                                        Text(' 82m',style: TextStyle(fontWeight: FontWeight.bold),)
-                                      ],
 
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  child: Container(
-                                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                    padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                    color: Colors.white70,
-                                    child: Row(
-                                      children : [
-                                        Icon(Icons.check_box_outlined),
-                                        Text(' 1/2',style: TextStyle(fontWeight: FontWeight.bold),)
-                                      ],
+            ),
 
-                                    ),
-                                  ),
-                                ),
-                              ],
-
-                            ),
-
-                          ],
-                        ),
-                      ),
-
-                    ],
-
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xffE4E7F2),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(22.0),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 550,
-                width : 380,
-                height: 200,
-                child: Container(
-                  child: Row(
-                    children : [
-                      Container(
-                        padding:EdgeInsets.fromLTRB(15, 0, 5, 70),
-                        child : ClipRRect(
-                          borderRadius: BorderRadius.circular(45.0),
-                          child : Image.asset(
-                            'imgs/daiso-log.png',width: 70.0,
-                          ),
-
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        child:Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('다이소',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,height: 2.2,),),
-                            Text('물티슈, 돌돌이 외 4개 ',style: TextStyle(fontSize: 15,),),
-                            Row(
-                              children: [
-                                SizedBox(
-                                  child: Container(
-                                    margin: EdgeInsets.fromLTRB(0, 10, 10, 0),
-                                    padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                    color: Colors.white70,
-                                    child: Row(
-                                      children : [
-                                        Icon(Icons.arrow_upward_outlined),
-                                        Text(' 212m',style: TextStyle(fontWeight: FontWeight.bold),)
-                                      ],
-
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  child: Container(
-                                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                    padding: EdgeInsets.fromLTRB(10, 5,10, 5),
-                                    color: Colors.white70,
-                                    child: Row(
-                                      children : [
-                                        Icon(Icons.check_box_outlined),
-                                        Text(' 0/6',style: TextStyle(fontWeight: FontWeight.bold),)
-                                      ],
-
-                                    ),
-                                  ),
-                                ),
-                              ],
-
-                            ),
-
-                          ],
-                        ),
-                      ),
-
-                    ],
-
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFFEBEE),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(22.0),
-                    ),
-                  ),
-                ),
-              ),
-            ],
           ),
-        ),
+
+
+        ],
+
       ),
     );
   }
 }
+
---
+++ src/lib/todo_sample.dart
@@ -0,0 +1,9 @@
+
+class TodoSample{
+  final String storeName;
+  final String todoText;
+  final String imgPath;
+  bool isDone;
+
+  TodoSample(this.storeName,  this.todoText, this.imgPath, bool bool,{this.isDone=false});
+}
